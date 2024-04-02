c85b899793dfd28d8fe5fc4cf918ece7ead12712
SaqibAli47 <bsit2017047@student.gudgk.edu.pk>
2024-04-02 15:13:40 +0500

    make foldable

--- src/components/navbar/links/Links.tsx
+++ src/components/navbar/links/Links.tsx
@@ -10,49 +10,49 @@ function PageLinks({ links }: { links: navbarProps[] }) {
     const [mobileMenu, setMobileMenu] = useState(false)
     const session = true;
     const isAdmin = true;
-  return (
-    <div className={styles.container}>
-        <div className={styles.mainLink}>
-        {links && links.length > 0 ? (
-            <div className={styles.links}>
-            {links.map((link, index) => (
-                <NavLink item={link} key={index} />
-            ))} {
-                session ? (
-                    <>
-                    {isAdmin && <NavLink item={{title: "Admin", slug: "/admin"}} />}
-                    <button className={styles.logout}>Logout</button>
-                    </>
+    return (
+        <div className={styles.container}>
+            <div className={styles.mainLink}>
+            {links && links.length > 0 ? (
+                <div className={styles.links}>
+                {links.map((link, index) => (
+                    <NavLink item={link} key={index} />
+                ))} {
+                    session ? (
+                        <>
+                        {isAdmin && <NavLink item={{title: "Admin", slug: "/admin"}} />}
+                        <button className={styles.logout}>Logout</button>
+                        </>
+                    ) : (
+                        <NavLink item={{title: "Login", slug: "/login"}}/>
+                    )
+                }
+                </div>
                 ) : (
-                    <NavLink item={{title: "Login", slug: "/login"}}/>
+                    <NotFound />
+                )}
+            </div>
+            {/* <button className={styles.menuButton} onClick={() => setMobileMenu((prevState) => !prevState)}>Menu</button> */}
+            <Image src={`/menu.png`} onClick={() => setMobileMenu((prevState) => !prevState)} className={styles.menuButton} alt="menu-png" width={30} height={30} />
+            {
+                mobileMenu && (
+                    <div className={styles.mobileLinks} >
+                        { links && links.map((link, index) => (
+                            <NavLink item={link} key={index} />
+                        ))} {
+                            session ? (
+                                <>
+                                {isAdmin && <NavLink item={{title: "Admin", slug: "/admin"}} />}
+                                <button className={styles.logout}>Logout</button>
+                                </>
+                            ) : (
+                                <NavLink item={{title: "Login", slug: "/login"}}/>
+                            )
+                        }
+                    </div>
                 )
             }
-            </div>
-            ) : (
-                <NotFound />
-            )}
         </div>
-        {/* <button className={styles.menuButton} onClick={() => setMobileMenu((prevState) => !prevState)}>Menu</button> */}
-        <Image src={`/menu.png`} onClick={() => setMobileMenu((prevState) => !prevState)} className={styles.menuButton} alt="menu-png" width={30} height={30} />
-        {
-            mobileMenu && (
-                <div className={styles.mobileLinks} >
-                    { links && links.map((link, index) => (
-                        <NavLink item={link} key={index} />
-                    ))} {
-                        session ? (
-                            <>
-                            {isAdmin && <NavLink item={{title: "Admin", slug: "/admin"}} />}
-                            <button className={styles.logout}>Logout</button>
-                            </>
-                        ) : (
-                            <NavLink item={{title: "Login", slug: "/login"}}/>
-                        )
-                    }
-                </div>
-            )
-        }
-    </div>
-  )
+    )
 }
 export default PageLinks;
